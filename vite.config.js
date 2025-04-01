5444e8b37f4cc510db0c74db1fe586141ec3deac
Robert Bischoff <bischrob@gmail.com>
2025-04-01 09:08:33 -0700

    added base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/cleanup/',  // <--- This is the key change
   plugins: [react()],
   build: {
     outDir: 'dist',  // output folder for the built site
